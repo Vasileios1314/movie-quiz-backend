6f7e0116d6d62f6218ce3f7bc928be87f831d3c6
poojadalai <poojadalai@gmail.com>
2022-05-02 13:13:08 +0200

    added endpoints

---
+++ routers/quiz.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+const Category = require("../models/").category;
+const Answer = require("../models/").answer;
+const Question = require("../models/").question;
+const router = new Router();
+
+//get category by id
+router.get("/category/:id", async (req, res, next) => {
+  try {
+    const categoryId = req.params.id;
+    const response = await Category.findByPk(categoryId, {
+      include: { model: Question, include: Answer },
+    });
+    if (response) {
+      res.json(response);
+    } else {
+      res.status(404).send("category not found");
+    }
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+
+//get all the question with answers
+router.get("/questions", async (req, res, next) => {
+  try {
+    const response = await Question.findAll({ include: Answer });
+    if (response) {
+      res.json(response);
+    } else {
+      res.status(404).send("Questions not found");
+    }
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
+module.exports = router;
