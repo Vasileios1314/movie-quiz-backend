634e35aa17242162fc998840391ed17066ecd901
t0mc0llins <tomcollins93@gmail.com>
2022-05-10 17:34:38 +0200

    removed unused routes

--- routers/quiz.js
+++ routers/quiz.js
@@ -1,42 +1,6 @@
 const { Router } = require("express");
-const Category = require("../models/").category;
-const Answer = require("../models/").answer;
-const Question = require("../models/").question;
 const Scoreboard = require("../models").scoreboard;
 const router = new Router();
-
-//get category by id
-router.get("/category/:id", async (req, res, next) => {
-  try {
-    const categoryId = req.params.id;
-    const response = await Category.findByPk(categoryId, {
-      include: { model: Question, include: Answer },
-    });
-    if (response) {
-      res.json(response);
-    } else {
-      res.status(404).send("category not found");
-    }
-  } catch (e) {
-    console.log(e.message);
-    next(e);
-  }
-});
-
-//get all the question with answers
-router.get("/questions", async (req, res, next) => {
-  try {
-    const response = await Question.findAll({ include: Answer });
-    if (response) {
-      res.json(response);
-    } else {
-      res.status(404).send("Questions not found");
-    }
-  } catch (e) {
-    console.log(e.message);
-    next(e);
-  }
-});
 
 // add new player score
 
@@ -54,23 +18,6 @@ router.get("/scoreboard", async (req, res, next) => {
   try {
     const response = await Scoreboard.findAll({ order: [["score", "DESC"]] });
     res.status(201).send(response);
-  } catch (e) {
-    console.log(e.message);
-    next(e);
-  }
-});
-
-router.get("/question/:id", async (req, res, next) => {
-  try {
-    const questionId = req.params.id;
-    const response = await Question.findByPk(questionId, {
-      include: Answer,
-    });
-    if (response) {
-      res.json(response);
-    } else {
-      res.status(404).send("Question not found");
-    }
   } catch (e) {
     console.log(e.message);
     next(e);
